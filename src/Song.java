7ad8b28f1edcdc052efbbb54c3ef805e51e3cfcb
persabellasirak <persabella.sirak@gmail.com>
2025-02-15 15:29:10 -0600

    Song.java

--- src/Song.java
+++ src/Song.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Song {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Song {
+    private String title;
+    private String artist;
+    private double length;
+
+    public Song(String title, String artist, double length) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "{ songName: " + title + ", artist: " + artist + ", length: " + length + " }";
     }
 }
